c5f92989577954b93823d1956fdd5a67e3aacbc6
munificentissimus1 <ray.csmarques@gmail.com>
2015-06-09 02:14:08 +0000

    REST POST Aluno

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
 var http = require("http");
 var path = require("path");
 var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var server = http.createServer(app);
+var mongoose = require("mongoose");
 
-var router = express();
-var server = http.createServer(router);
+//Modelo 
+var Aluno = require("./model/aluno.js")(mongoose).Aluno; // Modelo
+var Grupo = require("./model/grupo.js")(mongoose).Grupo; // Modelo
+
+//Services
+var registroService = require("./services/registroService.js")(Aluno);
+
+//Conectar ao banco de dados
+mongoose.connect('mongodb://munificentissimus1-prointeriv-1576013:27017/test');
 
 /*
   Configura o express para provimento de arquivos 
 */
-router.use(express.static(path.resolve(__dirname,'client')));
+app.use(express.static(path.resolve(__dirname,'client')));
 
-router.get('/teste',function(req,res){
-  res.send('Olá mundo');
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// REST Web Service - POST https://prointeriv-munificentissimus1.c9.io/alunos
+// Cadastrar novo aluno
+app.post('/alunos', jsonParser, function(req,res){
+  //Define o conteúdo a ser devolvido ao cliente
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  
+  var json = '';
+  registroService.registrarAluno(req.body.matricula, req.body.nome, req.body.nome, function(err){
+    if(err){
+      if (err.tipoErro === "duplicidade"){
+        res.statusCode = 409;
+      } else {
+        res.statusCode = 500;
+      }
+      json = JSON.stringify(err);
+      res.end(json);
+    }
+    res.end(JSON.stringify({"mensagem":"Aluno registrado com sucesso!"}));
+  });
+});
+
+// REST Web Service - GET https://prointeriv-munificentissimus1.c9.io/alunos 
+// Listar todos os alunos cadastrados
+app.get('/alunos', jsonParser, function(req,res){
+  
 });
 
 server.listen(process.env.PORT || 3000,process.env.IP || "0.0.0.0", 
